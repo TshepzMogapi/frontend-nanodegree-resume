e893add9992d1397fc00619cde5ee73b8bc3ca0a
Tshepiso Mogapi <Tshepiso Mogapi>
2017-08-14 19:35:05 +0200

    initial commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,105 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+
+var bio = 
+{
+	"name" : "Tshepiso Mogapi",
+	"role" : "Software Developer",
+	"contacts" : [
+	{
+		"mobile" : "+27 781891067",
+		"email" : "tshepzmogapi@gmail.com",
+		"github" : "tshepzmogapi",
+		"location": "Johannesburg"
+
+	}
+	],
+	"pictureURL" : "images/pic.gif",
+	"welcomeMessage" : "Halooo",
+	"skills" : ["UX", "Coding", "Problem solving"]
+}
+
+var work = 
+{
+	"jobs": [{
+			"title": "Software Developer",
+			"employer": "Microsoft",
+			"dates": "2017 April - current",
+			"location": "Johannesburg",
+			"description": "C#, Angular2, Material Design"
+		},
+		{
+
+			"title": "Mentor",
+			"employer": "Hyperion Development",
+			"dates": "2017 March - 2017 April",
+			"location": "Johannesburg",
+			"description": "Mentoring students in Python, JavaScript, HTML, CSS and Compiling course content"
+		},
+		{
+
+			"title": "Trainee User Experience Designer ",
+			"employer": "Johannesburg Centre For Software Engineering",
+			"dates": "2016 October - 2016 November",
+			"location": "Johannesburg",
+			"description": "Wireframing, Prototyping, Sketching, Ideation workshops"
+		},
+		{
+
+			"title": "Trainee Software Engineer",
+			"employer": "Copious Software",
+			"dates": "2016 February - 2016 June",
+			"location": "Johannesburg",
+			"description": "SQL, Magic XPA"
+		},
+		{
+
+			"title": "Administrative Support",
+			"employer": "ESB international",
+			"dates": "2015 February - 2015 December",
+			"location": "Johannesburg",
+			"description": "Assisting and troubleshooting computer related issues, Admin support"
+		}
+	]
+
+}
+
+var education =
+{
+	"schools" : [
+	{
+		"name" : "Wits University",
+		"location" : "Braamfontein",
+		"degreeDates" : "2010 - 2015",
+		"url" : "wits.ac.za",
+		"majors" : ["Physics", "Engineering Mechanics", "Mathematics"]
+	},
+	{
+		"name" : "Tswelelopele High",
+		"location" : "Mafikeng",
+		"degreeDates" : "2006 - 2008",
+		"url" : "",
+		"majors" : ["Physics", "Engineering Mechanics", "Mathematics"]
+	
+	}
+	],
+	"onlineCourses" : [
+	{
+		"title" : "JavaScript Basics",
+		"school": "Udacity",
+		"dates" : "2016 October - 2016 November",
+		"url" : "udacity.com"
+	}
+	]
+}
+
+var projects = 
+{
+	"projects" : [
+	{
+		"title" : "HTML and CSS",
+		"dates" : "2016 February",
+		"description" : "Building a basic web page based on Intro to HTML and CSS course by Udacity",
+		"images" : ["images/projectHTML.png"]
+	}
+	]
+}
